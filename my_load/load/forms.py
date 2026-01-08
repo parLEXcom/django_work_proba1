d7a81fb05e750aa69ac5549608c09bd86b51cf11
LEX327 <LEX327@yandex.ru>
2026-01-08 16:04:09 +0300

    Добавлена кнопка перехода на форму ввода на странице.

--- my_load/load/forms.py
+++ my_load/load/forms.py
@@ -1,13 +1,11 @@
 from django import forms
-
 from load.models import Post
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
-        fields= ['title', 'cover']
+        fields = ['title', 'cover']
 
 
 
